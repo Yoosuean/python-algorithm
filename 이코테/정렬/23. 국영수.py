be076c187aa296d21e50a95745d177a9432e7a9f
Yoosuean <awetumnn@gmail.com>
2023-05-14 23:18:03 +0900

    230514

--- 이코테/정렬/23. 국영수.py
+++ 이코테/정렬/23. 국영수.py
@@ -6,6 +6,6 @@ arr=[]
 for _ in range(n):
     name,a,b,c=input().split()
     arr.append((name,int(a),int(b),int(c)))
-    arr.sort(key=lambda x : (-x[1],x[2],-x[3],x[0]))
+arr.sort(key=lambda x : (-x[1],x[2],-x[3],x[0]))
 for i in arr:
     print(i[0])
